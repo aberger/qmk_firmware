404f09c5f2bacd3f92b5a9bcb152a905aff6d6ea
Alex Berger <alex@aberger.me>
2022-05-27 18:42:51 +0200

    Update saka68 layout

--- keyboards/cmm_studio/saka68/solder/keymaps/saka-68-alex/keymap.c
+++ keyboards/cmm_studio/saka68/solder/keymaps/saka-68-alex/keymap.c
@@ -18,9 +18,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_68_ansi(
-        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,            KC_DEL,  KC_ESC,
-        KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,        KC__MUTE,  KC_APPLE_FN,
-        MO(1),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,            KC_APPLE_FN,  KC_DEL,
+        KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,           KC__MUTE,  MO(1),
+        KC_ESC,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,
         KC_LCTL,   KC_LALT,   KC_LGUI,                       KC_SPC,                              KC_RGUI,   KC_RALT,     KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* [0]
 * ,--------------------------------------------------------------------------------------------------------.
-* | ESC  |  1!  |  2@  |  3#  |  4$  |  5%  |  6^  |  7&  |  8*  |  9(  |  0)  |  _-  |  +=  |     BSCP    |    ,-------------.
-* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|    |  FN  | MUTE |
+* |  `~  |  1!  |  2@  |  3#  |  4$  |  5%  |  6^  |  7&  |  8*  |  9(  |  0)  |  _-  |  +=  |     BSCP    |    ,-------------.
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|    |  FN  | DEL  |
 * | TAB    |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |  O  |   P   |  [{  |  }]  |    |\     |    |------+------|
-* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|    |  `~  | DEL  |
-* | ALT       |   A  |   S  |  D   |  F   |  G   |  H   |  J   |  K   |  L  |   :;  |  "'  |       ENT     |    `-------------'
+* |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|    | MUTE |  MO  |
+* | ESC       |   A  |   S  |  D   |  F   |  G   |  H   |  J   |  K   |  L  |   :;  |  "'  |       ENT     |    `-------------'
 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|           ,------.
 * | SHIFT       |   Z  |  X   |  C   |  V   |  B   |  N   |  M   |  <,  |  >.  |  ?/  |        SHIFT       |           |  UP  |
 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|    ,------+------+------.
-* | CTRL | ALT | CMD |                              SPC                                 | CMD |MO(1)| CTRL |    | LEFT | DOWN | RGHT |
+* | CTRL | ALT | CMD |                              SPC                                 | CMD | ALT | CTRL |    | LEFT | DOWN | RGHT |
 * `--------------------------------------------------------------------------------------------------------'    `--------------------'
 */
 
